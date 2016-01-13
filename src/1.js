467d90db4822071c0e6adb60f919443d8a2a292c
scotchfield <swrittenb@gmail.com>
2016-01-13 14:11:06 -0500

    Rewrite using a generator

--- src/1.js
+++ src/1.js
@@ -1,12 +1,18 @@
 'use strict'
 
-const n = 1000
+function* values () {
+	const n = 1000
+	for ( let i of Array( n ).keys() ) {
+		if ( 0 === i % 3 || 0 === i % 5 ) {
+			yield i
+		}
+	}
+}
+
 let total = 0
 
-for ( let i of Array( n ).keys() ) {
-	if ( 0 === i % 3 || 0 === i % 5 ) {
-		total += i
-	}
+for ( let x of values() ) {
+	total += x
 }
 
 console.log( total )
