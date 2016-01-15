48d06b7574b7d64decf82bfa810ccf16ae9bfbb0
scotchfield <swrittenb@gmail.com>
2016-01-14 21:15:30 -0500

    Update solution for Problem 3

--- src/3.js
+++ src/3.js
@@ -1,28 +1,31 @@
-(function () {
-    'use strict';
+'use strict';
 
-    var prime = function (n) {
-        var i;
-        if (n < 3) {
-            return true;
-        }
-        for (i = 2; i < n / 2 + 1; i += 1) {
-            if (0 === n % i) {
-                return false;
-            }
-        }
-        return true;
-    };
+function* range( start, max ) {
+    for ( let i = start; i <= max; i += 1 ) {
+        yield i
+    }
+}
 
-    return function (n) {
-        var i, p;
-        for (i = 2; i < Math.sqrt(n); i += 1) {
-            if (0 === n % i) {
-                p = prime(i);
-                if (true === p) {
-                    console.log(i);
-                }
-            }
+function prime( x ) {
+    if ( x < 2 ) {
+        return false
+    } else if ( x === 2 ) {
+        return true
+    }
+
+    for ( let i of range( 2, Math.sqrt( x ) ) ) {
+        if ( 0 === x % i ) {
+            return false
         }
-    };
-}())(600851475143);
+    }
+
+    return true
+}
+
+const n = 600851475143
+
+for ( let x of range( 2, Math.sqrt( n ) ) ) {
+    if ( 0 === n % x && prime( x ) ) {
+        console.log( x )
+    }
+}
