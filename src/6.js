12c7522dabac4b2a38a7e6631d4b126df8c57720
scotchfield <swrittenb@gmail.com>
2016-01-14 22:47:25 -0500

    Update Problem 6

--- src/6.js
+++ src/6.js
@@ -1,24 +1,24 @@
-console.log((function () {
-    'use strict';
+import 'babel-polyfill'
 
-    var SumSquares = function (n) {
-        var total = 0;
-        while (n > 0) {
-            total += n * n;
-            n -= 1;
-        }
-        return total;
-    },
-    SquareSum = function (n) {
-        var total = 0;
-        while (n > 0) {
-            total += n;
-            n -= 1;
-        }
-        return total * total;
-    };
+let sum_squares = ( n ) => {
+    let total = 0
+    while ( n > 0 ) {
+        total += n * n
+        n -= 1
+    }
+    return total
+}
 
-    return function (n) {
-        return SquareSum(n) - SumSquares(n);
-    };
-}())(100));
+let square_sum = ( n ) => {
+    let total = 0
+    while ( n > 0 ) {
+        total += n
+        n -= 1
+    }
+    return total * total
+}
+
+const n = 100
+let result = square_sum( n ) - sum_squares( n )
+
+console.log( result )
