764d7cb2e4a42a41045a14f08d4a019145022789
scotchfield <swrittenb@gmail.com>
2016-01-14 21:57:35 -0500

    Update Problem 4

--- src/4.js
+++ src/4.js
@@ -1,33 +1,24 @@
-(function () {
-    'use strict';
+import { range } from './util'
 
-    var x, y, total, largest = {'x': 0, 'y': 0, 'total': 0},
-
-    palindrome = function (x) {
-        var i;
-
-        if (typeof x !== 'string') {
-            return false;
-        }
-
-        for (i = 0; i < x.length / 2; i += 1) {
-            if (x[i] !== x[x.length - i - 1]) {
-                return false;
-            }
-        }
-
-        return true;
-    };
-
-    for (x = 100; x < 1000; x += 1) {
-        for (y = 100; y < 1000; y += 1) {
-            total = x * y;
-            if (total > largest.total && palindrome(String(total))) {
-                largest = {'x': x, 'y': y, 'total': total};
-            }
+function palindrome ( p ) {
+    for ( let i of range( 0, p.length / 2 ) ) {
+        if ( p[i] !== p[p.length - i - 1] ) {
+            return false
         }
     }
 
-    console.log(largest);
+    return true
+}
 
-}());
+let largest = 0
+
+for ( let x of range( 100, 999 ) ) {
+    for ( let y of range( 100, 999 ) ) {
+        let total = x * y
+        if ( total > largest && palindrome( String( total ) ) ) {
+            largest = total
+        }
+    }
+}
+
+console.log( largest )
